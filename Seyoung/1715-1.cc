445977888612099825e1a19409cdd02e3f15fda0
Seyoung Choi <tpdude0601@gmail.com>
2020-10-04 22:54:40 +0900

    둘다 메모리 초과 남..

---
+++ Seyoung/1715-1.cc
@@ -0,0 +1,34 @@
+
+#include <vector>
+#include <algorithm>
+#include <stdio.h>
+#include <queue>
+
+using namespace std;
+
+int main(void)
+{
+	//freopen("input.txt", "rt", stdin);
+	int n, sum = 0, a;
+	priority_queue<int, vector<int>, greater<int> > pQ;
+	scanf("%d", &n);
+
+	int x, y;
+
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		pQ.push(a); //최소 힙 
+	}
+
+	for (int i = 0; i < n; i++) {
+		x = pQ.top(); pQ.pop();
+		y = pQ.top(); pQ.pop();
+
+		sum = sum + x + y;
+		
+		if (pQ.empty()) break;
+		pQ.push(x + y);
+	}
+
+	printf("%d", sum);
+}
