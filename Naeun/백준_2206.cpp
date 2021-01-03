0db901135865676799be715a8c76c8f1ebabcceb
sne12345 <51853700+sne12345@users.noreply.github.com>
2021-01-04 05:34:50 +0900

    Add files via upload

---
+++ Naeun/백준_2206.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
+int main(void) {
+    
+    
+    
+    return 0;
+}
